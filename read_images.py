b47b3426cc03553b52ddbdea9af45c9d1dc071e2
Nate Jones <natejones031@gmail.com>
2019-12-23 18:44:52 -0500

    Added more stuff

--- read_images.py
+++ read_images.py
@@ -1,6 +1,11 @@
-import os
-import praw
 import getpass
+import os
+import requests
+
+import cv2 as cv
+import praw
+
+from proprossesing import prepocesses_image
 
 
 def download_images_from_subreddit(subreddit, number_images, save_path):
@@ -8,26 +13,39 @@ def download_images_from_subreddit(subreddit, number_images, save_path):
     Takes a subreddit name, for example "earthporn", downloads images from that subreddit,
     and saves them in <save_path>.
 
-    Note: The meta data for the images has to be saved and be able to relate back to the image.
-    Metadata we'll need includes number of upvotes, date posted, and post title.
+    Note: save_path must exist.
 
     :param subreddit: string. Subreddit instance to download from.
     :param save_path: string. Path to save downloaded images.
     :return: None
     """
-    # TODO configure where we want images from. ie: hot, top, new, etc
+    if(not os.path.exists(save_path)):
+        raise OSError("Save path does not exist!")
+
     # TODO figure out where we want to save images and the metadata
-    for submission in subreddit.hot(limit=number_images):
-        print(submission.title)  # Output: the submission's title
-        print(submission.score)  # Output: the submission's score
-        print(submission.id)  # Output: the submission's ID
-        print(submission.url)  # Output: the URL the submission points to
-        # or the submission's URL if it's a self post
+    count = 0
+    for submission in subreddit.top(limit=number_images):
+        # The top post in r/earthporn is an ad for fighting net neutrality.
+        if(count == 0):
+            count += 1
+            continue
+
+        image_path = "{}{}image_{}_score_{}.png".format(save_path,
+                                                        os.path.sep,
+                                                        submission.id,
+                                                        submission.score)
+
+        url = (submission.url)
+
+        r = requests.get(url)
+        with open(image_path, "wb") as f:
+            f.write(r.content)
 
 
 def get_subreddit_instance(subreddit, secret_file_path):
     """
 
+    :param secret_file_path:
     :param subreddit: string. Name of a subreddit.
     :return: The subreddit instance.
     """
@@ -44,17 +62,28 @@ def get_subreddit_instance(subreddit, secret_file_path):
     return reddit.subreddit(subreddit)
 
 
-def save_images_to_dropbox():
-    # TODO are we using dropbox? googledrive? something else?
-    raise NotImplementedError
-
-
 def main():
-    user = getpass.getuser()
+    number_images = 5
+
+    user             = getpass.getuser()
     secret_file_path = os.path.join("C:\\", "users", user, "Documents", "reddit_app_pass.txt")
+    image_save_path  = os.path.join("C:\\", "users", user, "Documents", "learning_earthporn", "images")
+
+    os.makedirs(image_save_path, exist_ok=True)
 
     subreddit = get_subreddit_instance("earthporn", secret_file_path)
-    download_images_from_subreddit(subreddit, 10, "")
+    download_images_from_subreddit(subreddit, number_images, image_save_path)
+
+    files           = os.listdir(image_save_path)
+    test_image_path = os.path.join(image_save_path, files[0])
+
+    image      = cv.imread(test_image_path)
+    post_image = prepocesses_image(image)
+
+    cv.imshow("pre test", image)
+    cv.imshow("post test", post_image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 
 if( __name__ == "__main__"):
